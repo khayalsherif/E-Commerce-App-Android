f3b176866329b1e4f660051a2e1070fc6d7ba70b
khayalsharifli <xeyalsherif@gmail.com>
2023-01-05 12:16:26 +0400

    first

---
+++ app/src/main/java/az/red/e_commerce_app_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package az.red.e_commerce_app_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
