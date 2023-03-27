41472bfb569742a7d55d3836b6dcc5d39be660b6
khayalsharifli <xeyalsherif@gmail.com>
2023-03-27 19:22:47 +0400

    complete detail screen ui

--- presentation/src/main/java/az/red/presentation/content/productDetail/ProductDetailFragment.kt
+++ presentation/src/main/java/az/red/presentation/content/productDetail/ProductDetailFragment.kt
@@ -2,9 +2,16 @@ package az.red.presentation.content.productDetail
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import az.red.domain.common.NetworkResult
 import az.red.presentation.base.BaseFragment
 import az.red.presentation.databinding.FragmentProductDetailBinding
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.models.SlideModel
+import kotlinx.coroutines.launch
 import kotlin.reflect.KClass
 
 class ProductDetailFragment : BaseFragment<FragmentProductDetailBinding, ProductDetailViewModel>() {
@@ -12,10 +19,35 @@ class ProductDetailFragment : BaseFragment<FragmentProductDetailBinding, Product
         get() = FragmentProductDetailBinding::inflate
     override val kClass: KClass<ProductDetailViewModel>
         get() = ProductDetailViewModel::class
+    private lateinit var navController: NavController
 
     private val args by navArgs<ProductDetailFragmentArgs>()
 
     override val bindViews: FragmentProductDetailBinding.() -> Unit = {
+        navController = findNavController()
+        toolbar.setNavigationOnClickListener { navController.popBackStack() }
         viewModel.getProductDetail(args.id)
+
+
+        lifecycleScope.launch {
+            viewModel.productResponse.collect {
+                when (it) {
+                    is NetworkResult.Empty -> {}
+                    is NetworkResult.Error -> {
+                        showToast(it.message!!)
+                    }
+                    is NetworkResult.Exception -> {
+                        showToast(it.message!!)
+                    }
+                    is NetworkResult.Loading -> {}
+                    is NetworkResult.Success -> {
+                        val list = it.data!!.imageUrls.map { image -> SlideModel(image ) }
+                        imageSlider.setImageList(list,ScaleTypes.FIT)
+                    }
+                }
+            }
+        }
+
+
     }
 }
--- presentation/src/main/java/az/red/presentation/content/productDetail/ProductDetailViewModel.kt
+++ presentation/src/main/java/az/red/presentation/content/productDetail/ProductDetailViewModel.kt
@@ -2,23 +2,23 @@ package az.red.presentation.content.productDetail
 
 import androidx.lifecycle.viewModelScope
 import az.red.domain.common.NetworkResult
+import az.red.domain.model.auth.login.Login
+import az.red.domain.model.product.Product
 import az.red.domain.usecase.home.GetProductByIdUseCase
 import az.red.presentation.base.BaseViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 
 class ProductDetailViewModel(private val useCase: GetProductByIdUseCase) : BaseViewModel() {
 
+    private val _productResponse =
+        MutableStateFlow<NetworkResult<Product>>(NetworkResult.Empty())
+    val productResponse: StateFlow<NetworkResult<Product>> get() = _productResponse
+
     fun getProductDetail(id: String) = viewModelScope.launch {
         useCase.invoke(id).collect {
-            when (it) {
-                is NetworkResult.Empty -> {}
-                is NetworkResult.Error -> {}
-                is NetworkResult.Exception -> {}
-                is NetworkResult.Loading -> {}
-                is NetworkResult.Success -> {
-                    println(it.data)
-                }
-            }
+            _productResponse.emit(it)
         }
     }
 }
